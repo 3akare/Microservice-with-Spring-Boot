f633a1d7263f64d001f9733e7cee467162a9e3c4
3akare <bakaredavid007@gmail.com>
2024-10-06 12:13:05 +0100

    build: switch back to h2 database dependency to reduce docker load during development

--- cards/src/main/java/com/microservice/cards/constant/CardsConstants.java
+++ cards/src/main/java/com/microservice/cards/constant/CardsConstants.java
@@ -6,7 +6,7 @@ public class CardsConstants {
     public static final String  CREDIT_CARD = "Credit Card";
     public static final int  NEW_CARD_LIMIT = 1_00_000;
     public static final String STATUS_201 = "201";
-    public static final String MESSAGE_201 = "Account created successfully";
+    public static final String MESSAGE_201 = "Card created successfully";
     public static final String STATUS_200 = "200";
     public static final String MESSAGE_200 = "Request Processed successfully";
     public static final String STATUS_500 = "500";
